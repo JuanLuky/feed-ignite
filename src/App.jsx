da7a017c48385df6560397eba80d5a453f8c77bb
Juan Carlos <juandev33@gmail.com>
2022-06-18 16:42:57 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const posts = [
       {type: 'paraghaph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
       {type: 'link', content:'👉 jane.design/doctorcare'},
     ],
-    publishedAt: new Date('2022-06-14 18:00:00'),
+    publishedAt: new Date('2022-06-17 18:00:00'),
   },
   {
     id: 2,
